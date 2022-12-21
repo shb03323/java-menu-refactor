c0633c9e7aa889e0c042e30d9bab12bf743601f4
jeonghoon <shb03323@naver.com>
2022-12-21 15:28:44 +0900

    feat(Coach): 코치의 이름 길이 예외 처리 기능 추가

---
+++ src/main/java/menu/constant/CoachConstant.java
@@ -0,0 +1,9 @@
+package menu.constant;
+
+public class CoachConstant {
+
+    public static final int COACH_NAME_MAX_LENGTH = 4;
+    public static final int COACH_NAME_MIN_LENGTH = 2;
+    public static final int COACH_MAX_COUNT = 5;
+    public static final int COACH_MIN_COUNT = 2;
+}
--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -3,16 +3,30 @@ package menu.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import static menu.constant.CoachConstant.COACH_NAME_MAX_LENGTH;
+import static menu.constant.CoachConstant.COACH_NAME_MIN_LENGTH;
+import static menu.constant.SystemConstant.ERROR_PREFIX;
+
 public class Coach {
 
     private final String coachName;
     private final List<Menu> dislikeMenus = new ArrayList<>();
 
     public Coach(String coachName) {
+        validateCoachNameLength(coachName);
         this.coachName = coachName;
     }
 
     public boolean isSameName(String coachName) {
         return this.coachName.equals(coachName);
+    }
+
+    private void validateCoachNameLength(String coachName) {
+        if (coachName.length() > COACH_NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException(ERROR_PREFIX + "코치의 이름은 최대 4글자 입니다.");
+        }
+        if (coachName.length() < COACH_NAME_MIN_LENGTH) {
+            throw new IllegalArgumentException(ERROR_PREFIX + "코치의 이름은 최소 2글자 입니다.");
+        }
     }
 }
