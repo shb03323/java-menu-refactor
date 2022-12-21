a1517617ab7414a57130b292266386231d3c8983
jeonghoon <shb03323@naver.com>
2022-12-21 16:57:56 +0900

    feat: 못 먹는 메뉴 입력 기능 추가

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -10,9 +10,17 @@ public class InputView {
 
     public static final String INPUT_SPLIT_OPTION = ",";
     private static final String INPUT_COACHES_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private static final String INPUT_DISLIKE_MENUS = "(이)가 못 먹는 메뉴를 입력해 주세요.";
 
     public static List<String> inputCoachesName() {
         System.out.println(INPUT_COACHES_NAME);
+        String input = Console.readLine();
+        System.out.println();
+        return parseToSplitOption(input);
+    }
+
+    public static List<String> inputDislikeMenusName(String coachName) {
+        System.out.println(coachName + INPUT_DISLIKE_MENUS);
         String input = Console.readLine();
         System.out.println();
         return parseToSplitOption(input);
