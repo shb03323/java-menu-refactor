5b4bfb3e53f5aa336a077b733fc32d56b82abe0b
jeonghoon <shb03323@naver.com>
2022-12-21 16:56:58 +0900

    feat: 메뉴 이름 중복 입력 시 예외 처리 기능 추가

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -2,6 +2,7 @@ package menu.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static menu.constant.CoachConstant.COACH_NAME_MAX_LENGTH;
 import static menu.constant.CoachConstant.COACH_NAME_MIN_LENGTH;
@@ -18,6 +19,7 @@ public class Coach {
     }
 
     public void addDislikeMenus(List<Menu> dislikeMenus) {
+        validateSameDislikeMenu(dislikeMenus);
         this.dislikeMenus.addAll(dislikeMenus);
     }
 
@@ -35,6 +37,15 @@ public class Coach {
         }
         if (coachName.length() < COACH_NAME_MIN_LENGTH) {
             throw new IllegalArgumentException(ERROR_PREFIX + "코치의 이름은 최소 2글자 입니다.");
+        }
+    }
+
+    private void validateSameDislikeMenu(List<Menu> menus) {
+        List<Menu> distinctMenus = menus.stream()
+                .distinct()
+                .collect(Collectors.toUnmodifiableList());
+        if (distinctMenus.size() < menus.size()) {
+            throw new IllegalArgumentException(ERROR_PREFIX + "메뉴 이름을 중복으로 입력하셨습니다.");
         }
     }
 }
