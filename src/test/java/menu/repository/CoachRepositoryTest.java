5b0309a52dd61cd43ae677677cc8d9c59f5f2d57
jeonghoon <shb03323@naver.com>
2022-12-22 20:39:13 +0900

    test(CoachRepositoryTest): 코치들의 점심 메뉴 추천 목록 문자열 형태로 잘 변형되는지 확인

--- src/test/java/menu/repository/CoachRepositoryTest.java
+++ src/test/java/menu/repository/CoachRepositoryTest.java
@@ -1,5 +1,8 @@
 package menu.repository;
 
+import menu.domain.Category;
+import menu.domain.Coach;
+import menu.domain.Menu;
 import menu.domain.repository.CoachRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,7 +10,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -22,13 +27,6 @@ public class CoachRepositoryTest {
         coachRepository = new CoachRepository();
     }
 
-    private static Stream<Arguments> generateCoachesNameData() {
-        return Stream.of(
-                Arguments.of(List.of("후니")),
-                Arguments.of(List.of("후니", "포비", "쥬니", "레아", "지우", "루피"))
-        );
-    }
-
     @Test
     void 코치_생성_테스트() {
         coachRepository.addCoaches(List.of("후니", "포비"));
@@ -40,6 +38,13 @@ public class CoachRepositoryTest {
         coachRepository.addCoaches(List.of("후니", "포비"));
         coachRepository.clearCoaches();
         assertEquals(coachRepository.getCoaches().size(), 0);
+    }
+
+    private static Stream<Arguments> generateCoachesNameData() {
+        return Stream.of(
+                Arguments.of(List.of("후니")),
+                Arguments.of(List.of("후니", "포비", "쥬니", "레아", "지우", "루피"))
+        );
     }
 
     @ParameterizedTest
@@ -56,5 +61,21 @@ public class CoachRepositoryTest {
         assertThatThrownBy(() -> coachRepository.addCoaches(coachesName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
+    }
+
+    @Test
+    void 코치들의_점심_메뉴_추천_목록_제대로_들고오는지_확인() {
+        coachRepository.addCoaches(List.of("후니", "건모"));
+        Menu menu = new Menu("우아한", Category.KOREAN);
+        Coach coach1 = coachRepository.getCoaches().get(0);
+        Coach coach2 = coachRepository.getCoaches().get(1);
+        coach1.addRecommendMenu(menu);
+        coach2.addRecommendMenu(menu);
+
+        Map<String, List<String>> recommendMenusName = new LinkedHashMap<>();
+        recommendMenusName.put("후니", coach1.getRecommendMenusName());
+        recommendMenusName.put("건모", coach2.getRecommendMenusName());
+
+        assertEquals(coachRepository.getRecommendMenusName(), recommendMenusName);
     }
 }
