b9a9454a252937e76556b187379b7c60de2f0f78
jeonghoon <shb03323@naver.com>
2022-12-22 20:43:37 +0900

    refactor(Days): sequence 필드 제거

--- src/main/java/menu/domain/Days.java
+++ src/main/java/menu/domain/Days.java
@@ -2,17 +2,15 @@ package menu.domain;
 
 public enum Days {
 
-    MONDAY(1, "월요일"),
-    TUESDAY(2, "화요일"),
-    WEDNESDAY(3, "수요일"),
-    THURSDAY(4, "목요일"),
-    FRIDAY(5, "금요일");
+    MONDAY("월요일"),
+    TUESDAY("화요일"),
+    WEDNESDAY("수요일"),
+    THURSDAY("목요일"),
+    FRIDAY("금요일");
 
-    private final int sequence;
     private final String dayName;
 
-    Days(int sequence, String dayName) {
-        this.sequence = sequence;
+    Days(String dayName) {
         this.dayName = dayName;
     }
 
